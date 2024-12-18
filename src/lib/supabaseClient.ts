34bc73ab91435a1d84cdff763f23b3db6edf81bc
David Beshensky <beshenskydave@gmail.com>
2024-12-18 12:41:36 -0600

    new stuff

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,4 +4,9 @@ import { Database } from '@/types/supabase';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Supabase URL and Key are required.");
+  }
+
+
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
