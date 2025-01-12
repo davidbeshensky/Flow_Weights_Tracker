1d8fdf298b2d9539ee8e97d7c5b16f12efe19c38
David Beshensky <beshenskydave@gmail.com>
2025-01-12 10:52:40 -0600

    new things

--- src/components/CalendarHeatmap.tsx
+++ src/components/CalendarHeatmap.tsx
@@ -161,7 +161,7 @@ export default function MonthlyCalendarHeatmap() {
 }
 
 function getCalendarGridDates(year: number, month: number) {
-  const dates = [];
+  const dates: { dateStr: string; isCurrentMonth: boolean; }[] = [];
   const firstDay = new Date(year, month, 1);
   const lastDay = new Date(year, month + 1, 0); // Last day of the month
 
